08280c08223cac514c3793675d90c3908d1fb3f9
lillygrogono <145426987+lillygrogono@users.noreply.github.com>
2024-09-10 12:49:21 -0400

    Add files via upload

---
+++ unit-1/lesson-1/command_line_output.py
@@ -0,0 +1,24 @@
+# i = 1
+#while i < 10:
+#    if i % 2 == 0: 
+       # print(i)
+#  i = i + 1
+
+# the % symbol checks to see if the # is divided by 2 it's zero.
+
+#i = 1
+#while i < 10:
+ #   if i % 2 != 0:  
+   #     print(i)
+   # i = i + 1
+
+# the ! tells the code to show all answers that are not even
+
+i = -10
+while i < 0:  
+    print(i)
+    i = i + 1  
+    
+# pretty self explanatory
+
+
