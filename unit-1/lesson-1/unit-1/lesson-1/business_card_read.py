76e99eacf20cd421d99950dd03c1afb5f2f7f294
lillygrogono <145426987+lillygrogono@users.noreply.github.com>
2024-09-10 12:41:52 -0400

    Add files via upload

---
+++ unit-1/lesson-1/unit-1/lesson-1/business_card_read.py
@@ -0,0 +1,15 @@
+# Print a blank line
+print("\n")
+
+# Ask the user for a filename
+filename = input("Please enter the name of a file in this directory: ")
+
+# Open that file
+file = open(filename)
+
+print("Name is: " + file.readline())
+print("Title is: " + file.readline())
+print("Phone number is: " + file.readline())
+print("Address is: " + file.readline())
+
+#I'm not really sure what I'm doing wrong but it's giving me an error message
