0c0eb407a677eeaad2db13bfad83286dd8a9f668
lillygrogono <145426987+lillygrogono@users.noreply.github.com>
2024-09-17 13:08:29 -0400

    Delete unit-1/lesson-3 directory

--- unit-1/lesson-3/lesson-3/manovich-bot.py
+++
@@ -1,8 +0,0 @@
-import sys 
-from manovichdata import *
-
-userInput = sys.argv[1]
-
-if userInput == "VARIABILITY":
-     print(VARIABILITY[0])
-     exit()
--- unit-1/lesson-3/lesson-3/manovichdata.py
+++
@@ -1,6 +0,0 @@
-NUMERICAL_REPRESENTATION = ["Everything is a number.", "Sampling and quantization.",]
-MODULARITY = ["A collection of shapes, sounds, and images to piece together art.", "New media objects are made up of independent, self-sufficient parts.", "Central to digital media's flexibility."]
-AUTOMATION = ["Computers automate or mimic human thought.", "Algorithmic culture.", "Minizing human effort."]
-VARIABILITY = ["A concept that allows the computer to reshape media.", "Everything is recycled", "New media is about versions not about originality."]
-TRANSCODING = ["Altered from one form into another"]
-
